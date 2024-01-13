47a04ad67e2d7e0ae779a61dcff3cdc19e59d387
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-01-14 00:04:19 +0100

    recent changes on signup

--- src/app/pre-login-components/signup/signup.component.ts
+++ src/app/pre-login-components/signup/signup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-// import { AngularFireAuth } from '@angular/fire/compat/auth';
-// import { AuthService } from '../../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AuthService } from '../../services/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
@@ -14,6 +14,7 @@ export class SignupComponent implements OnInit {
 
   signupForm!: FormGroup;
   firebaseErrorMessage: string;
+  authService: any;
 
   constructor(
     // public authService: AuthService,
@@ -28,24 +29,24 @@ export class SignupComponent implements OnInit {
       username: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', Validators.required),
-      // accept-terms: new FormControl('', Validators.requiredTrue)
+      acceptTerms: new FormControl(false, Validators.requiredTrue)
     });
   }
 
-  // async signup(): Promise<void> {
-  //   if (this.signupForm.invalid) {
-  //     return;
-  //   }
+  public signup() {
+    if (this.signupForm.invalid) {
+      return;
+    }
 
-  //   try {
-  //     const result: any = await this.authService.signupUser(this.signupForm.value);
-  //     if (result == null) {
-  //       this.router.navigate(['/home']);
-  //     } else if (result.isValid == false) {
-  //       this.firebaseErrorMessage = result.message;
-  //     }
-  //   } catch (error: any) {
-  //     // Handle error
-  //   }
-  // }
+    try {
+      const result: any = this.authService.signupUser(this.signupForm.value);
+      if (result == null) {
+        this.router.navigate(['/home']);
+      } else if (result.isValid == false) {
+        this.firebaseErrorMessage = result.message;
+      }
+    } catch (error: any) {
+      // Handle error
+    }
+  }
 }
