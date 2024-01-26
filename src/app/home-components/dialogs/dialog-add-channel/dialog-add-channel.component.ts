19c6006f9146aa3cfce3a61355c7cc8161f4705e
Timo Kempf <124527843+Timokinator@users.noreply.github.com>
2024-01-26 14:34:09 +0100

    add creator to channel

--- src/app/home-components/dialogs/dialog-add-channel/dialog-add-channel.component.ts
+++ src/app/home-components/dialogs/dialog-add-channel/dialog-add-channel.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Channel } from '../../../../models/channel.class';
 import {
   query,
@@ -16,20 +16,67 @@ import {
   DocumentReference,
 } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
+import { User } from '../../../../models/user.class';
 
 @Component({
   selector: 'app-dialog-add-channel',
   templateUrl: './dialog-add-channel.component.html',
   styleUrl: './dialog-add-channel.component.scss',
 })
-export class DialogAddChannelComponent {
+export class DialogAddChannelComponent implements OnInit {
+
   channel: Channel = new Channel();
   formIncomplete: boolean = false; //muss später true sein
   loading: boolean = false;
 
   firestore: Firestore = inject(Firestore);
+  storedUserAuthUID: any;
+  user: User = new User();
 
-  constructor(public dialog: MatDialog) {}
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.storedUserAuthUID = sessionStorage.getItem('userAuthUID');
+    this.getUser();
+
+  }
+
+  getUser() {
+    let q;
+    if (this.storedUserAuthUID) {
+      q = query(this.getUsersRef(), where("authUID", "==", this.storedUserAuthUID));
+    } else {
+      q = query(this.getUsersRef(), where("authUID", "==", this.storedUserAuthUID)); // q = input für Gastzugang
+    }
+
+    return onSnapshot(q, (docSnap: any) => {
+      docSnap.forEach((doc: any) => {
+        this.user = new User(this.setUserObject(doc.data()))
+      })
+    })
+  }
+
+
+  getUsersRef() {
+    return collection(this.firestore, 'users');
+  }
+
+
+  setUserObject(obj: any) {
+    return {
+      id: obj.id || "",
+      authUID: obj.authUID || "",
+      name: obj.name || "",
+      status: obj.status || true,
+      avatarURL: obj.avatarURL || '',  // code added Ben
+      photoURL: obj.photoURL || '',
+      channels: obj.channels || [],
+      email: obj.email || ''
+    }
+  }
+
+
 
   saveChannel() {
     this.loading = true;
@@ -41,7 +88,7 @@ export class DialogAddChannelComponent {
       id: id || '',
       chanName: obj.chanName || '',
       description: obj.description || '',
-      creator: obj.creator || '',
+      creator: this.setUserObject(this.user) || '',
       users: obj.users || [],
       posts: obj.posts || [],
     };
@@ -56,18 +103,18 @@ export class DialogAddChannelComponent {
         .then((docRef) => {
           this.loading = false;
           const newID = docRef?.id;
-          this.updateChannelWithId(item, newID);
+          this.updateChannelWithId(newID);
         });
     }
   }
 
-  async updateChannelWithId(item: Channel, newId: any) {
+  async updateChannelWithId(newId: any) {
     const docRef = doc(this.getChannelRef(), newId);
     await updateDoc(docRef, { id: newId })
       .catch((err) => {
         console.log(err);
       })
-      .then(() => {});
+      .then(() => { });
   }
 
   getChannelRef() {
@@ -77,4 +124,10 @@ export class DialogAddChannelComponent {
   closeDialog() {
     this.dialog.closeAll();
   }
+
+
+
+
+
+
 }
